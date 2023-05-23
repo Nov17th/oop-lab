038d2b30701bdc5ea51123ed73eab86fdbc812d4
Ha Chu Minh <Ha.CM210293@sis.hust.edu.vn>
2023-05-24 03:31:47 +0700

    Add completed work for Lab 01
    - Includes answers.txt file for lab questions
    - Added Lab01 project source code for section 2 of assignment
    - Added JavaBasics project source code for section 6 of assignment

---
+++ Laboratory 01/JavaBasics/src/DisplayTriangle.java
@@ -0,0 +1,22 @@
+// Exercise 6.3: Write a program to display a triangle with a height of n stars (*), n is entered by users
+
+import java.util.Scanner;
+
+public class DisplayTriangle 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Enter the height: ");
+        int input = keyboard.nextInt();
+        keyboard.close();
+        
+        for (int i = 0; i < input; ++i)
+        {
+            System.out.println(" ".repeat(input - i - 1) + "*".repeat(i * 2 + 1));
+        }
+	}
+
+}
