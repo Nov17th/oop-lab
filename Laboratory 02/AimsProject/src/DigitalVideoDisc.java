9137e4393fb013314314b995a7e881a982bcbaec
Ha Chu Minh <Ha.CM210293@sis.hust.edu.vn>
2023-05-27 18:23:22 +0700

    Added unique id and instance count

--- Laboratory 02/AimsProject/src/DigitalVideoDisc.java
+++ Laboratory 02/AimsProject/src/DigitalVideoDisc.java
@@ -1,43 +1,55 @@
 public class DigitalVideoDisc 
 {
+
 	private String title;
 	private String category;
 	private String director;
 	private int length;
 	private float cost;
 	
+	private static int nbDigitalVideoDiscs = 0;
+	private int id = 0;
+
 	// Constructors
 	public DigitalVideoDisc(String title) 
 	{
 		super();
+		nbDigitalVideoDiscs += 1;
 		this.title = title;
+		this.id = nbDigitalVideoDiscs;
 	}
 	
 	public DigitalVideoDisc(String title, String category, float cost) 
 	{
 		super();
+		nbDigitalVideoDiscs += 1;
 		this.title = title;
 		this.category = category;
 		this.cost = cost;
+		this.id = nbDigitalVideoDiscs;
 	}
 
 	public DigitalVideoDisc(String title, String category, String director, float cost) 
 	{
 		super();
+		nbDigitalVideoDiscs += 1;
 		this.title = title;
 		this.category = category;
 		this.director = director;
 		this.cost = cost;
+		this.id = nbDigitalVideoDiscs;
 	}
 	
 	public DigitalVideoDisc(String title, String category, String director, int length, float cost) 
 	{
 		super();
+		nbDigitalVideoDiscs += 1;
 		this.title = title;
 		this.category = category;
 		this.director = director;
 		this.length = length;
 		this.cost = cost;
+		this.id = nbDigitalVideoDiscs;
 	}
 	
 	// Getters and setters
@@ -71,4 +83,9 @@ public class DigitalVideoDisc
 		return cost;
 	}
 	
+	public int getId() 
+	{
+		return id;
+	}
+
 }
